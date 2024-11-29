802a8e5ef77e63ee287469e145694cf9473c8013
EmhaHasyim <redeath001@gmail.com>
2024-11-29 11:42:39 +0700

    change folder name Server to server

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import {useState} from "react";
 import {Button} from "@/components/ui/button"
-import {AppType} from "../../Server";
+import {AppType} from "../../server";
 import {hc} from 'hono/client'
 
 const client = hc<AppType>('/')
