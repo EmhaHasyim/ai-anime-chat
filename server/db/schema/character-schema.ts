d90a36c67c5b1c6d812eb7e0c4882add0d2f0744
EmhaHasyim <redeath001@gmail.com>
2024-12-01 12:28:04 +0700

    Update character table schema: - Change primary key from id to uuid - Add created_at and edited_at timestamp columns - Rename existing columns for better clarity

--- server/db/schema/character-schema.ts
+++ server/db/schema/character-schema.ts
@@ -1,12 +1,14 @@
-import {integer, pgTable, varchar, text, bigint} from "drizzle-orm/pg-core";
+import {integer, pgTable, varchar, text, bigint, timestamp, uuid} from "drizzle-orm/pg-core";
 
 export const characterTable = pgTable('characters', {
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    id: uuid('id').defaultRandom(),
     name: varchar({length: 255}).notNull(),
     description: varchar({length: 255}).notNull(),
     gender: varchar({length: 6}).notNull(),
     img: varchar({length: 255}).notNull(),
-    totalChat: bigint({mode: "number"}).default(0).notNull(),
-    aiCommand: text().notNull(),
+    total_chat: bigint({mode: "number"}).default(0).notNull(),
+    ai_command: text().notNull(),
+    created_at: timestamp().defaultNow().notNull(),
+    edited_at: timestamp().defaultNow().notNull()
 });
 
