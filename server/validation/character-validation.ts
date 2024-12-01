7b9f5766ae4400cd9dca38f1dd268ae74d67c9c2
EmhaHasyim <redeath001@gmail.com>
2024-12-01 10:06:29 +0700

    add validation for get route

--- server/validation/character-validation.ts
+++ server/validation/character-validation.ts
@@ -1,5 +1,12 @@
 import {z} from "zod";
 
+const validateCharacterSelect = z.object({
+    sort: z.enum(['name', 'chat_count', 'newest']).optional(),
+    gender: z.enum(['male', 'female', 'both']).optional(),
+    order: z.enum(['asc', 'desc']).optional(),
+    name: z.string().optional()
+})
+
 const validateCharacterInsert = z.object({
     name: z.string().min(1).max(255),
     description: z.string().min(1).max(255),
