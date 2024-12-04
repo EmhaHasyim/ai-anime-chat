0ad6847a23b62aaf12617e5371fe1bdecaafab33
EmhaHasyim <redeath001@gmail.com>
2024-12-04 13:34:36 +0700

    update schema definition for SQLite compatibility

--- server/db/schema/character-schema.ts
+++ server/db/schema/character-schema.ts
@@ -1,14 +1,16 @@
-import {integer, pgTable, varchar, text, bigint, timestamp, uuid} from "drizzle-orm/pg-core";
+import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
+import { sql } from "drizzle-orm";
 
-export const characterTable = pgTable('characters', {
-    id: uuid('id').defaultRandom(),
-    name: varchar({length: 255}).notNull(),
-    description: varchar({length: 255}).notNull(),
-    gender: varchar({length: 6}).notNull(),
-    img: varchar({length: 255}).notNull(),
-    total_chat: bigint({mode: "number"}).default(0).notNull(),
+const charactersTable = sqliteTable('characters', {
+    id: text().primaryKey().$defaultFn(() => crypto.randomUUID()),
+    name: text().notNull().unique(),
+    description: text().notNull(),
+    gender: text().notNull(),
+    img: text().notNull(),
+    total_chat: integer().notNull().default(0),
     ai_command: text().notNull(),
-    created_at: timestamp().defaultNow().notNull(),
-    edited_at: timestamp().defaultNow().notNull()
+    createdAt: text().notNull().default(sql`(CURRENT_TIMESTAMP)`),
+    editedAt: text().notNull().default(sql`(CURRENT_TIMESTAMP)`).$onUpdate(() => sql`(CURRENT_TIMESTAMP)`)
 });
 
+export default charactersTable
