d5154bdcbea369519afc5fd4dd249dc9ea4c875c
EmhaHasyim <redeath001@gmail.com>
2024-12-05 15:52:48 +0700

    add router and tanstack query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,25 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import {createRouter, RouterProvider} from "@tanstack/react-router";
+import {routeTree} from "./routes/__root.tsx"
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import "@fontsource/poppins/400.css"
+
+const router = createRouter({routeTree})
+
+declare module '@tanstack/react-router' {
+    interface Register {
+        router: typeof router
+    }
+}
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App/>
-    </StrictMode>,
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router}/>
+        </QueryClientProvider>
+    </StrictMode>
 )
