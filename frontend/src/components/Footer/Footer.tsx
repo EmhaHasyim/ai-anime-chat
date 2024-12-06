ace643891c1234696c8bee23b7ee72e63911d0bd
EmhaHasyim <redeath001@gmail.com>
2024-12-06 17:12:16 +0700

    change footer ux

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,9 +1,10 @@
 import NavigationBarFooter from "@/components/Footer/navigation/NavigationBarFooter.tsx";
 
 const Footer = () => {
+
     return (
         <>
-            <footer className={'flex justify-evenly items-center h-14 bg-neutral-900 w-full'}>
+            <footer className={'flex justify-evenly items-center h-11 bg-neutral-900 w-full fixed bottom-0 left-0'}>
                 <NavigationBarFooter/>
             </footer>
         </>
