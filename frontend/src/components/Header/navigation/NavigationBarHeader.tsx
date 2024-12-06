43c903eb8727ba70edccde570c8b67fa88acacc5
EmhaHasyim <redeath001@gmail.com>
2024-12-06 17:12:43 +0700

    add icon user to header

--- frontend/src/components/Header/navigation/NavigationBarHeader.tsx
+++ frontend/src/components/Header/navigation/NavigationBarHeader.tsx
@@ -1,14 +1,23 @@
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from "@/components/ui/avatar"
+
 const NavigationBarHeader = () => {
     return (
         <>
             <nav>
                 <ul>
                     <li>
-                        some feature
+                        <Avatar className={'scale-90'}>
+                            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn"/>
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
                     </li>
                 </ul>
             </nav>
         </>
     )
 }
-export  default NavigationBarHeader
+export default NavigationBarHeader
